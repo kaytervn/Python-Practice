a64aa59cde65d4f7358f2b4fe7545f3049b7e16c
KayterCantFixBug <kienductrong@gmail.com>
2024-03-14 19:52:09 +0700

    a

--- expected_time.py
+++
@@ -1,5 +0,0 @@
-a = int(input("a = "))
-b = int(input("b = "))
-m = int(input("m = "))
-
-print("ET: ", (a + 4 * m + b) / 6)
--- test_notes.py
+++ test_notes.py
@@ -2094,3 +2094,9 @@ m = int(input('m = '))
 
 print((a+4*m+b)/6)
 # %%
+a = int(input("a = "))
+b = int(input("b = "))
+m = int(input("m = "))
+
+print("ET: ", (a + 4 * m + b) / 6)
+# %%
